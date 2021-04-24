cff454434938036cfd7c4e6eabe69518477452e2
Cano4ek <hnenniy1610@gmail.com>
2021-04-24 11:20:44 +0300

    Initial Commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'black'}}>
+      <Text style={{color: 'white', fontSize: 24}}>ThrowNoMore</Text>
+    </View>
+  )
+}
+
+export default App;
---
+++ ios/Pods/Target Support Files/Pods-ThrowNoMore-ThrowNoMoreTests/Pods-ThrowNoMore-ThrowNoMoreTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ThrowNoMore_ThrowNoMoreTests : NSObject
+@end
+@implementation PodsDummy_Pods_ThrowNoMore_ThrowNoMoreTests
+@end
